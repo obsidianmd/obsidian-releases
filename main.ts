28898658816d54c0b9865f819914554600a076fb
lishid <lishid@gmail.com>
2020-10-26 11:07:18 -0400

    Refactor: Use new version of obsidian.d.ts from remote.

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
-import { App, CustomPlugin, Modal, Notice, PluginSettingTab, Setting } from 'obsidian';
+import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-export default class MyPlugin extends CustomPlugin {
+export default class MyPlugin extends Plugin {
 	onInit() {
 
 	}
