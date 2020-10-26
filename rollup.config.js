245339d8e08567a490df8dd5c2d25014539a8f99
lishid <lishid@gmail.com>
2020-10-26 11:58:14 -0400

    Use rollup for bundling.

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import typescript from '@rollup/plugin-typescript';
+import {nodeResolve} from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+  input: 'main.ts',
+  output: {
+    dir: '.',
+    sourcemap: 'inline',
+    format: 'cjs',
+    exports: 'default'
+  },
+  external: ['obsidian'],
+  plugins: [
+    typescript(),
+    nodeResolve({browser: true}),
+    commonjs(),
+  ]
+};
